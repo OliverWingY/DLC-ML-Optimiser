56efe19513c206aa6057f9d2f7d1f77c4d59db17
44742 <44742@DESKTOP-TEH3IHN>
2022-01-31 18:04:55 +0000

    Add project files.

---
+++ DlcCoatingOptimiser/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace DlcCoatingOptimiser
+{
+    
+    public class Program
+    {
+        private static MLApp.MLApp Matlab;
+        public static void Main(string[] args)
+        {
+            Initialise_App();
+
+        }
+
+        private static void Initialise_App()
+        {
+            Matlab = new MLApp.MLApp();
+            Matlab.Execute("cd C:\\Users\\44742\\Documents\\MATLAB\\IndividualProject");
+            Matlab.Feval("InitialiseMatlabWorkspace",1, out object MatlabInitialisationSuccessful);
+            Matlab.Feval("CheckWorkspace", 1, out object WorkspaceIsInitialised);
+
+            if ((bool)((object[])MatlabInitialisationSuccessful)[0])
+                Console.WriteLine("Matlab app successfully initialised");
+            if ((bool)((object[])WorkspaceIsInitialised)[0])
+                Console.WriteLine("Matlab Workspace initialised");
+            
+
+        }
+    }
+}
